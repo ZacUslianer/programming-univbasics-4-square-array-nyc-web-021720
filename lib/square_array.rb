a97c2d10486b68d31279318aa910d38dfe4879aa
Zac Uslianer <zacuslianer@gmail.com>
2020-02-02 21:50:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   result = []
   counter = 0 
-while counter < numbers.length
+while counter < numbers.size 
   puts numbers[counter] 
     counter += 1  
     result << number
