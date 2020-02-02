bec807799ee74640ac3eba6aff5faf0d9bfb05b2
Zac Uslianer <zacuslianer@gmail.com>
2020-02-02 21:45:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,11 @@ def square_array(numbers)
   result = []
   counter = 0 
 while counter < numbers.length
-  puts numbers[counter] ** 2 
+  puts numbers[counter] 
     counter += 1  
     numbers << result
+    result ** 2
   end
-  numbers
+  result
 end
 
